ab6827fc7f8538470bfa0b393406cdf80c356367
sriramsanjeev100 <sriramsanjeev100@gmail.com>
2026-07-23 12:16:27 +0530

    Parantheses Validator Executed

---
+++ CollectionsPackage/ParenthesesValidator.java
@@ -0,0 +1,56 @@
+package CollectionsPackage;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ParenthesesValidator
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Java Code : ");
+        String code = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+        boolean valid = true;
+
+        for (int i = 0; i < code.length(); i++)
+        {
+            char ch = code.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
+                stack.push(ch);
+            }
+            else if (ch == ')' || ch == '}' || ch == ']')
+            {
+                if (stack.isEmpty())
+                {
+                    valid = false;
+                    break;
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '['))
+                {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if (!stack.isEmpty())
+        {
+            valid = false;
+        }
+        if (valid)
+        {
+            System.out.println("Parentheses are Balanced.");
+        }
+        else
+        {
+            System.out.println("Invalid Parentheses.");
+        }
+
+        sc.close();
+    }
+}
