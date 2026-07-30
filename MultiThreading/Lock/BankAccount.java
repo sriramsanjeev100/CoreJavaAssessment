960cdaf3e99c8aea1830ccf09b81688576a71520
sriramsanjeev100 <sriramsanjeev100@gmail.com>
2026-07-30 18:13:47 +0530

    Lock Implemented

---
+++ MultiThreading/Lock/BankAccount.java
@@ -0,0 +1,54 @@
+package MultiThreading.Lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BankAccount
+{
+    private double balance;
+    Lock lock = new ReentrantLock();
+
+    public BankAccount(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public String deposit(String customerName, double amount)
+    {
+        lock.lock();
+        try
+        {
+            balance += amount;
+
+            return customerName + " deposited Rs." + amount + "\nCurrent Balance : Rs." + balance;
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+
+    public String withdraw(String customerName, double amount)
+    {
+        lock.lock();
+        try
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return customerName + " withdrew Rs." + amount + "\nCurrent Balance : Rs." + balance;
+            }
+
+            return customerName + " - Insufficient Balance";
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+}
---
+++ MultiThreading/Lock/BankMain.java
@@ -0,0 +1,54 @@
+package MultiThreading.Lock;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BankMain
+{
+    public static void main(String[] args)
+    {
+        BankAccount account = new BankAccount(20000);
+
+        System.out.println("===== Initial Balance =====");
+        System.out.println("Current Balance : Rs." + account.getBalance());
+        System.out.println();
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.execute(() ->
+        {
+            System.out.println(account.deposit("Sriram", 2000));
+            System.out.println();
+        });
+
+        executor.execute(() ->
+        {
+            System.out.println(account.withdraw("Leo", 1500));
+            System.out.println();
+        });
+
+        executor.execute(() ->
+        {
+            System.out.println(account.deposit("John", 5000));
+            System.out.println();
+        });
+
+        executor.execute(() ->
+        {
+            System.out.println(account.withdraw("Aaron", 4000));
+            System.out.println();
+        });
+
+        executor.execute(() ->
+        {
+            System.out.println(account.deposit("Sona", 1000));
+            System.out.println();
+        });
+
+        executor.shutdown();
+        while (!executor.isTerminated())
+        {        }
+
+        System.out.println("===== Final Balance =====");
+        System.out.println("Current Balance : Rs." + account.getBalance());
+    }
+}
