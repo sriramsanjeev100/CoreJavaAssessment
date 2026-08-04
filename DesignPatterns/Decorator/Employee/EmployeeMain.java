5d78c19d1b72e3245130f6644e88c074a37900e7
sriramsanjeev100 <sriramsanjeev100@gmail.com>
2026-08-04 16:08:52 +0530

    Decorator Implemented

---
+++ DesignPatterns/Decorator/Employee/EmployeeMain.java
@@ -0,0 +1,20 @@
+package DesignPatterns.Decorator.Employee;
+
+public class EmployeeMain
+{
+    public static void main(String[] args)
+    {
+        Employee employee = new BasicEmployee("Sriram", 50000);
+        employee = new BonusDecorator(employee);
+        employee = new InsuranceDecorator(employee);
+        employee = new TransportDecorator(employee);
+        employee = new MealCouponDecorator(employee);
+
+        System.out.println("===== Employee Details =====");
+        System.out.println(employee.getDetails());
+
+        System.out.println();
+
+        System.out.println("Final Salary : Rs." + employee.getSalary());
+    }
+}
