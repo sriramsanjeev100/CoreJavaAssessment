a998501b534cb26b99c85c53a322cb9fb598ba01
sriramsanjeev100 <sriramsanjeev100@gmail.com>
2026-08-07 17:42:23 +0530

    Jakarta Import fixed

--- LibraryManagementSystem/src/main/java/org/LibraryManagement/entity/Book.java
+++ LibraryManagementSystem/src/main/java/org/LibraryManagement/entity/Book.java
@@ -1,6 +1,9 @@
 package org.LibraryManagement.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Book
