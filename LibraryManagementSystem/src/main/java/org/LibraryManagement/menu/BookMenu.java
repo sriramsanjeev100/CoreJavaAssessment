e9e1af220a7d4e97f33d0d6a407847a15384eaa1
sriramsanjeev100 <sriramsanjeev100@gmail.com>
2026-08-07 11:11:33 +0530

    Implementing Library Management Main

---
+++ LibraryManagementSystem/src/main/java/org/LibraryManagement/menu/BookMenu.java
@@ -0,0 +1,203 @@
+package org.LibraryManagement.menu;
+
+import org.LibraryManagement.entity.Book;
+import org.LibraryManagement.service.BookService;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BookMenu
+{
+    private static final BookService service = new BookService();
+    public static void showMenu(Scanner sc)
+    {
+        int choice;
+        do
+        {
+            System.out.println("\n========== Book Management ==========");
+            System.out.println("1. Add Book");
+            System.out.println("2. Display All Books");
+            System.out.println("3. Search Book");
+            System.out.println("4. Update Book");
+            System.out.println("5. Delete Book");
+            System.out.println("6. BACK");
+            System.out.print("Enter Choice : ");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice)
+            {
+                case 1:
+                    addBook(sc);
+                    break;
+
+                case 2:
+                    displayBooks();
+                    break;
+
+                case 3:
+                    searchBook(sc);
+                    break;
+
+                case 4:
+                    updateBook(sc);
+                    break;
+
+                case 5:
+                    deleteBook(sc);
+                    break;
+
+                case 6:
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+
+        } while(choice != 6);
+    }
+
+    public static void addBook(Scanner sc)
+    {
+        System.out.print("Enter Title : ");
+        String title = sc.nextLine();
+
+        System.out.print("Enter Author : ");
+        String author = sc.nextLine();
+
+        System.out.print("Enter Category : ");
+        String category = sc.nextLine();
+
+        System.out.print("Enter Total Copies : ");
+        int totalCopies = sc.nextInt();
+
+        System.out.print("Enter Available Copies : ");
+        int availableCopies = sc.nextInt();
+
+        Book book = new Book(title, author, category, totalCopies, availableCopies);
+        service.createBook(book);
+        System.out.println("Book Added Successfully");
+    }
+
+    public static void displayBooks()
+    {
+        List<Book> books = service.findAllBooks();
+        if (books.isEmpty())
+        {
+            System.out.println("No Books Available");
+        }
+        else
+        {
+            System.out.println("\n========== BOOK LIST ==========");
+            for (Book b : books)
+            {
+                System.out.println(b);
+                System.out.println("--------------------------------");
+            }
+        }
+    }
+
+    public static void searchBook(Scanner sc)
+    {
+        System.out.print("Enter Book ID : ");
+        int searchId = sc.nextInt();
+
+        Book searchedBook = service.findBookById(searchId);
+        if (searchedBook != null)
+        {
+            System.out.println(searchedBook);
+        }
+        else
+        {
+            System.out.println("Book Not Found");
+        }
+    }
+
+    public static void updateBook(Scanner sc)
+    {
+        System.out.print("Enter Book ID to Update : ");
+        int updateId = sc.nextInt();
+        sc.nextLine();
+
+        Book book = service.findBookById(updateId);
+
+        if (book == null)
+        {
+            System.out.println("Book Not Found");
+            return;
+        }
+
+        int choice;
+
+        do
+        {
+            System.out.println("\n===== Update Book =====");
+            System.out.println("1. Update Title");
+            System.out.println("2. Update Author");
+            System.out.println("3. Update Category");
+            System.out.println("4. Update Total Copies");
+            System.out.println("5. Update Available Copies");
+            System.out.println("6. Save and Exit");
+            System.out.print("Enter Choice : ");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice)
+            {
+                case 1:
+
+                    System.out.print("Enter New Title : ");
+                    book.setTitle(sc.nextLine());
+                    break;
+
+                case 2:
+
+                    System.out.print("Enter New Author : ");
+                    book.setAuthor(sc.nextLine());
+                    break;
+
+                case 3:
+
+                    System.out.print("Enter New Category : ");
+                    book.setCategory(sc.nextLine());
+                    break;
+
+                case 4:
+
+                    System.out.print("Enter New Total Copies : ");
+                    book.setTotalCopies(sc.nextInt());
+                    sc.nextLine();
+                    break;
+
+                case 5:
+
+                    System.out.print("Enter New Available Copies : ");
+                    book.setAvailableCopies(sc.nextInt());
+                    sc.nextLine();
+                    break;
+
+                case 6:
+
+                    service.updateBook(book);
+                    System.out.println("Book Updated Successfully");
+                    break;
+
+                default:
+
+                    System.out.println("Invalid Choice");
+            }
+
+        } while (choice != 6);
+    }
+
+    public static void deleteBook(Scanner sc)
+    {
+        System.out.print("Enter Book ID to Delete : ");
+        int deleteId = sc.nextInt();
+
+        service.deleteBook(deleteId);
+        System.out.println("Book Deleted Successfully");
+    }
+}
---
+++ LibraryManagementSystem/src/main/java/org/LibraryManagement/menu/MemberMenu.java
@@ -0,0 +1,173 @@
+package org.LibraryManagement.menu;
+
+import org.LibraryManagement.entity.Member;
+import org.LibraryManagement.service.MemberService;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MemberMenu
+{
+    private static final MemberService service = new MemberService();
+
+    public static void showMenu(Scanner sc)
+    {
+        int choice;
+
+        do
+        {
+            System.out.println("\n========== Member Management ==========");
+            System.out.println("1. Add Member");
+            System.out.println("2. Display Members");
+            System.out.println("3. Search Member");
+            System.out.println("4. Update Member");
+            System.out.println("5. Delete Member");
+            System.out.println("6. BACK");
+            System.out.print("Enter Choice : ");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice)
+            {
+                case 1:
+                    addMember(sc);
+                    break;
+
+                case 2:
+                    displayMembers();
+                    break;
+
+                case 3:
+                    searchMember(sc);
+                    break;
+
+                case 4:
+                    updateMember(sc);
+                    break;
+
+                case 5:
+                    deleteMember(sc);
+                    break;
+
+                case 6:
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+
+        } while(choice != 6);
+    }
+
+    public static void addMember(Scanner sc)
+    {
+        System.out.print("Enter Member Name : ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Phone Number : ");
+        String phone = sc.nextLine();
+
+        Member member = new Member(name, phone);
+        service.createMember(member);
+        System.out.println("Member Added Successfully");
+    }
+
+    public static void displayMembers()
+    {
+        List<Member> members = service.findAllMembers();
+        if(members.isEmpty())
+        {
+            System.out.println("No Members Found");
+        }
+        else
+        {
+            for(Member member : members)
+            {
+                System.out.println(member);
+                System.out.println("-----------------------------");
+            }
+        }
+    }
+
+
+    public static void searchMember(Scanner sc)
+    {
+        System.out.print("Enter Member ID : ");
+        int memberId = sc.nextInt();
+        sc.nextLine();
+
+        Member member = service.findMemberById(memberId);
+        if(member != null)
+        {
+            System.out.println(member);
+        }
+        else
+        {
+            System.out.println("Member Not Found");
+        }
+    }
+
+    public static void updateMember(Scanner sc)
+    {
+        System.out.print("Enter Member ID : ");
+        int memberId = sc.nextInt();
+        sc.nextLine();
+
+        Member member = service.findMemberById(memberId);
+        if(member == null)
+        {
+            System.out.println("Member Not Found");
+            return;
+        }
+
+        int choice;
+        do
+        {
+            System.out.println("\n===== Update Member =====");
+            System.out.println("1. Update Name");
+            System.out.println("2. Update Phone");
+            System.out.println("3. Save and Exit");
+
+            System.out.print("Enter Choice : ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice)
+            {
+                case 1:
+
+                    System.out.print("Enter New Name : ");
+                    member.setMemberName(sc.nextLine());
+                    break;
+
+                case 2:
+
+                    System.out.print("Enter New Phone : ");
+                    member.setPhone(sc.nextLine());
+                    break;
+
+                case 3:
+
+                    service.updateMember(member);
+                    System.out.println("Member Updated Successfully");
+                    break;
+
+                default:
+
+                    System.out.println("Invalid Choice");
+            }
+
+        } while(choice != 3);
+    }
+
+    public static void deleteMember(Scanner sc)
+    {
+        System.out.print("Enter Member ID : ");
+        int memberId = sc.nextInt();
+        sc.nextLine();
+        service.deleteMember(memberId);
+        System.out.println("Member Deleted Successfully");
+
+    }
+}
